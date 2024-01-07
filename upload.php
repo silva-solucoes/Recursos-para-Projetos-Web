0e2f267fb798c6acd77bdd492d3a437d2b59ea69
Silva Soluções Tech <silvasolucoes2023@gmail.com>
2024-01-07 11:53:28 -0300

    Projeto Finalizado

--- upload.php
+++ upload.php
@@ -9,6 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $uploadedFile = $_FILES["uploaded-file"];
+    $studentName = $_POST["student_name"];
 
     // Verifica se houve algum erro no upload
     if ($uploadedFile["error"] !== UPLOAD_ERR_OK) {
@@ -16,27 +17,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    $targetFile = $uploadDir . basename($uploadedFile["name"]);
-
-    // Verifica se o arquivo já existe
-    if (file_exists($targetFile)) {
-        echo "Erro: O arquivo já existe.";
+    // Verifica o tipo de arquivo
+    $fileExtension = pathinfo($uploadedFile["name"], PATHINFO_EXTENSION);
+    if (strtolower($fileExtension) !== "pdf") {
+        echo "Erro: Apenas arquivos PDF são permitidos.";
         exit();
     }
 
-    // Verifica o tipo de arquivo, se necessário
-    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    if ($fileType != "pdf" && $fileType != "txt" && $fileType != "jpg") {
-        echo "Erro: Apenas arquivos PDF, TXT e JPG são permitidos.";
-        exit();
-    }
+    // Define a lógica de renomear o arquivo
+    $newFileName = 'Doc_' . date('ymd') . '_' . sprintf('%02d', getFileSequence($uploadDir)) . '_Ex_Estudante.' . $fileExtension;
+
+    $targetFile = $uploadDir . $newFileName;
 
     // Move o arquivo para o diretório de destino
     if (move_uploaded_file($uploadedFile["tmp_name"], $targetFile)) {
         // Se o upload foi bem-sucedido, você pode adicionar mais ações aqui, como:
         
         // 1. Atualizar a interface do usuário
-        echo "Arquivo enviado com sucesso!";
+        echo "Arquivo enviado com sucesso! Novo nome: $newFileName";
         
         // 2. Redirecionar para outra página
         // header("Location: outra_pagina.php");
@@ -46,5 +44,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 } else {
     echo "Método inválido.";
+}
+
+// Função para obter a sequência numérica
+function getFileSequence($directory)
+{
+    $files = glob($directory . 'Doc_*_*_*.*');
+    return count($files) + 1;
+}
+
+// Função para sanitizar o nome do arquivo removendo caracteres especiais
+function sanitizeFileName($fileName)
+{
+    $sanitizedFileName = preg_replace('/[^a-zA-Z0-9]/', '_', $fileName);
+    return $sanitizedFileName;
 }
 ?>
