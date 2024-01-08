046a3fbb057b9f2e8c48ded1b86c3b70621139c7
Silva Soluções Tech <silvasolucoes2023@gmail.com>
2024-01-07 21:44:12 -0300

    Finalizar

--- Modal-elemento-de-dialogo/app/Views/index.php
+++ Modal-elemento-de-dialogo/app/Views/index.php
@@ -8,28 +8,40 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.21.0/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo URL . '/public/css/style.css'; ?>">
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 
-<body> 
+<body>
     <button type="button" class="share-btn">
         <!--Socials-->
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+            <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+        </svg>
     </button>
     <dialog class="copy-link-dialog">
         <form>
             <header>
                 <h2>Compartilhe o link</h2>
-                <button type="button" class="close-btn secndary">
+                <button type="button" class="close-btn secondary">
                     <!--Botão Fechar-->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                    </svg>
                 </button>
             </header>
-            <div class="copy-link-wrapper">
-                <input id="copy-link-input" type="text" required value="https://example.com/share.this" readonly>
-                <button class="copy-btn" type="button">
-                    <!--copy icon-->
-                    <span id="copy-text">Link de cópia</span>
-                </button>
+            <div class="copy-link-dialog__content">
+                <div class="copy-link-wrapper">
+                    <input id="copy-link-input" type="text" required value="https://example.com/share.this" readonly>
+                    <button class="copy-btn" type="button">
+                        <!--copy icon-->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z" />
+                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
+                        </svg>
+                        <span id="copy-text">Link de cópia</span>
+                    </button>
+                </div>
             </div>
         </form>
     </dialog>
--- Modal-elemento-de-dialogo/public/js/script.js
+++ Modal-elemento-de-dialogo/public/js/script.js
@@ -1,190 +1,26 @@
-// Obtém a primeira instância da classe "dropzone-box"
-const dropZoneBox = document.getElementsByClassName("dropzone-box")[0];
+const modalDialog = document.querySelector(".copy-link-dialog");
+const shareBtn = document.querySelector(".share-btn");
+const closeBtn = document.querySelector(".close-btn");
+const copyBtn = document.querySelector(".copy-btn");
 
-// Seleciona todos os elementos de input com o tipo "file" dentro da classe "dropzone-area"
-const inputFiles = document.querySelectorAll(".dropzone-area input[type='file']");
-
-// Seleciona o primeiro elemento de input do tipo "file"
-const inputElement = inputFiles[0];
-
-// Encontra o elemento pai mais próximo com a classe "dropzone-area"
-const dropZoneElement = inputElement.closest(".dropzone-area");
-
-// Adiciona um ouvinte de evento para o evento de mudança no input do tipo "file"
-inputElement.addEventListener("change", (e) => {
-    if (inputElement.files.length) {
-        // Atualiza a lista de arquivos na área de dropzone
-        updateDropzoneFileList(dropZoneElement, inputElement.files[0]);
-    }
+shareBtn.addEventListener("click", () => {
+    modalDialog.classList.remove("copy-link-dialog--fadeout");
+    modalDialog.showModal();
 });
 
-// Adiciona um ouvinte de evento para o evento de arrastar sobre a área de dropzone
-dropZoneElement.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    // Adiciona uma classe para indicar que o cursor está sobre a área de dropzone
-    dropZoneElement.classList.add("dropzone--over");
+closeBtn.addEventListener("click", () => {
+    modalDialog.classList.add("copy-link-dialog--fadeout");
+    modalDialog.close();
 });
 
-// Adiciona ouvintes de evento para os eventos de "dragleave" e "dragend"
-["dragleave", "dragend"].forEach((type) => {
-    dropZoneElement.addEventListener(type, (e) => {
-        // Remove a classe que indica que o cursor não está mais sobre a área de dropzone
-        dropZoneElement.classList.remove("dropzone--over");
-    });
-});
+copyBtn.addEventListener("click", () => {
+    const copyInput = document.getElementById("copy-link-input");
 
-// Adiciona um ouvinte de evento para o evento de soltar (drop) na área de dropzone
-dropZoneElement.addEventListener("drop", (e) => {
-    e.preventDefault();
-    if (e.dataTransfer.files.length) {
-        // Define os arquivos da transferência de dados como os arquivos do input do tipo "file"
-        inputElement.files = e.dataTransfer.files;
-        // Atualiza a lista de arquivos na área de dropzone
-        updateDropzoneFileList(dropZoneElement, e.dataTransfer.files[0]);
-    }
-    // Remove a classe que indica que o cursor não está mais sobre a área de dropzone
-    dropZoneElement.classList.remove("dropzone--over");
-});
+    copyInput.select();
+    copyInput.setSelectionRange(0, 99999);
 
-// Função para atualizar a lista de arquivos na área de dropzone
-const updateDropzoneFileList = (dropZoneElement, file) => {
-    // Obtém o elemento dentro da área de dropzone que exibe a mensagem sobre os arquivos
-    let dropzoneFileMessage = dropZoneElement.querySelector(".message");
+    navigator.clipboard.writeText(copyInput.value);
 
-    // Atualiza a mensagem para incluir o nome do arquivo e o tamanho em bytes
-    dropzoneFileMessage.innerHTML = `${file.name}, ${file.size} bytes`;
-};
-
-// Adiciona um ouvinte de evento para o evento de reset no formulário
-dropZoneBox.addEventListener("reset", (e) => {
-    // Limpa as mensagens sobre os arquivos na área de dropzone
-    let dropzoneFileMessage = dropZoneElement.querySelector(".message");
-    dropzoneFileMessage.innerHTML = "Nenhum arquivo selecionado";
-});
-
-// Adiciona um ouvinte de evento para o evento de submit no formulário
-dropZoneBox.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // Exibe no console o primeiro arquivo selecionado
-    const myFiled = document.getElementById("upload-file");
-    console.log(myFiled.files[0]);
-});
-
-$(document).ready(function () {
-    // Aguarda o documento HTML ser totalmente carregado antes de executar o código
-
-    $(".dropzone-box").submit(function (e) {
-        // Adiciona um ouvinte de evento para o evento de envio do formulário
-        e.preventDefault();
-
-        // Impede o envio padrão do formulário para evitar recarregamento da página
-
-        var formData = new FormData(this);
-        // Cria um objeto FormData para coletar dados do formulário, incluindo arquivos
-
-        var file = $("#upload-file")[0].files[0];
-        // Obtém o primeiro arquivo selecionado no input do tipo file
-
-        if (file && file.type === "application/pdf") {
-            // Verifica se um arquivo foi selecionado e se é do tipo PDF
-
-            $.ajax({
-                type: "POST",
-                // Define o método HTTP da requisição como POST
-
-                url: "http://localhost/UploadArquivos/paginas/uploadPDF",
-                // Especifica o URL para o qual a requisição será enviada
-
-                data: formData,
-                // Define os dados a serem enviados, incluindo o arquivo, utilizando o FormData
-
-                contentType: false,
-                processData: false,
-                // Configurações para garantir que o conteúdo não seja processado ou alterado pelo jQuery
-
-                success: function (response) {
-                    // Função a ser executada em caso de sucesso na requisição AJAX
-
-                    var fileName = file.name;
-                    // Exibindo as mensagens e redefinindo o input do tipo file após o upload
-                    $(".dropzone-area p").show();
-                    $(".message").text("Nenhum arquivo selecionado").css("color", "");
-                    $("#upload-file").val("");
-
-                    // Exibindo um alerta personalizado
-                    showAlert("Upload de arquivo realizado com sucesso!", "success");
-                },
-                error: function (error) {
-                    $(".message").html("<strong>Erro:</strong> Por favor, selecione um arquivo PDF.").css("color", "red");
-                    showAlert("Erro ao enviar o arquivo.", "error");
-                    console.log(error);
-                },
-            });
-        } else {
-            $(".message").html("<strong>Erro:</strong> Por favor, selecione um arquivo PDF.").css("color", "red");
-            // Limpa o texto da mensagem e a cor do texto
-        }
-    });
-
-    $("#upload-file").change(function () {
-        // Adiciona um ouvinte de evento para o evento de mudança no input do tipo file
-
-        var file = $("#upload-file")[0].files[0];
-        // Obtém o primeiro arquivo selecionado no input do tipo file
-
-        if (file && file.type === "application/pdf") {
-            var fileName = file.name;
-            $(".message").text("Arquivo selecionado: " + fileName).css("color", "green");
-            // Exibe a mensagem indicando o arquivo selecionado e define a cor do texto como verde
-        } else {
-            $(".message").text("Por favor, insira um arquivo PDF").css("color", "red");
-            // Exibe a mensagem indicando que apenas arquivos PDF são aceitos e define a cor do texto como vermelha
-        }
-        $("#upload-success, #upload-failed").hide();
-        // Esconde as mensagens de sucesso e falha, se estiverem visíveis
-    });
-
-    $(".dropzone-area").on("dragover", function () {
-        // Adiciona um ouvinte de evento para o evento de arrastar sobre a área de dropzone
-
-        $("#upload-success, #upload-failed").hide();
-        // Esconde as mensagens de sucesso e falha, se estiverem visíveis
-    });
-
-    $(".dropzone-box").on("reset", function () {
-        // Adiciona um ouvinte de evento para o evento de reset do formulário
-
-        $(".dropzone-area p").show();
-        // Exibe o parágrafo dentro da área de dropzone
-
-        $(".message").text("Nenhum arquivo selecionado").css("color", "");
-        // Define o texto da mensagem de volta para o estado inicial e limpa a cor do texto
-
-        $("#upload-file").val("");
-        // Limpa o valor do input do tipo file
-
-        $("#upload-success, #upload-failed").hide();
-        // Esconde as mensagens de sucesso e falha, se estiverem visíveis
-    });
-
-    function showAlert(message, type) {
-        // Cria e exibe um alerta personalizado
-
-        var alertBox = $("<div>").addClass("custom-alert " + type).text(message);
-        var closeButton = $("<span>").addClass("close-button").text("Fechar");
-
-        alertBox.append(closeButton);
-        $("body").append(alertBox);
-
-        closeButton.click(function () {
-            alertBox.remove();
-        });
-    }
-
-    $(".dropzone-box").on("reset", function () {
-        $(".dropzone-area p").show();
-        $(".message").text("Nenhum arquivo selecionado").css("color", "");
-        $("#upload-file").val("");
-        $("#upload-success, #upload-failed").hide();
-    });
+    const copyText = document.getElementById("copy-text");
+    copyText.innerHTML = "Copiado!";
 });
